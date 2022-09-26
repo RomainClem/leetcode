0c2eb9310156baf081a45dea7e1d11f2cec79135
Romain Clemencon <romain.clemencon@mycit.ie>
2022-09-26 23:56:08 +0100

    Starting working on q105

--- Trees/Kth Smallest Element in a BST/sol.py
+++ Trees/Kth Smallest Element in a BST/sol.py
@@ -9,4 +9,17 @@ class TreeNode:
         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        q = [root]
+        dif = 0
+        if not root.right and not root.left: return root.val
+        while len(q) - dif <= k:
+            if q[-1].left: q.append(q[-1].left)
+            elif q[-1].right: q.append(q[-1].right)
+            else: 
+                n = q.pop()
+                if q[-1].left == n: q[-1].left = None
+                else: q[-1].right = None
+                dif+=1
+                
+        return q[-(k-dif)].val
         
