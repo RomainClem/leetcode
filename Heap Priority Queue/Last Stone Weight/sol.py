bfd93173af11e3abf533239c9960b3a9a8b82ea4
Romain Clemencon <romain.clemencon@mycit.ie>
2022-10-13 23:59:59 +0100

    Doing more heap yip yip

--- Heap Priority Queue/Last Stone Weight/sol.py
+++ Heap Priority Queue/Last Stone Weight/sol.py
@@ -1,3 +1,12 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        
+        stones = [-s for s in stones]
+        heapify(stones)
+
+        while len(stones) > 1:
+            first = -(heappop(stones))
+            second = -(heappop(stones))
+            if first > second:
+                heappush(stones, -(first-second))
+
+        return -(heappop(stones)) if len(stones) > 0 else 0
